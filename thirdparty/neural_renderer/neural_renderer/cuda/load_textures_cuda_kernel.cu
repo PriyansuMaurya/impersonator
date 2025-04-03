f3dccf8521ce460ec6a3641b7295c067b7ce2420
Priyanshu Maurya <priyanshu1999pm@gmail.com>
2025-04-03 12:18:41 +0530

    update load_textures_cuda_kernel.cu

--- thirdparty/neural_renderer/neural_renderer/cuda/load_textures_cuda_kernel.cu
+++ thirdparty/neural_renderer/neural_renderer/cuda/load_textures_cuda_kernel.cu
@@ -1,5 +1,4 @@
 #include <ATen/ATen.h>
-
 #include <cuda.h>
 #include <cuda_runtime.h>
 
@@ -20,10 +19,10 @@ __global__ void load_textures_cuda_kernel(
   }
   const int ts = texture_size;
   const int fn = i / (ts * ts * ts);
-  scalar_t dim0 = ((i / (ts * ts)) % ts) / (ts - 1.) ;
-  scalar_t dim1 = ((i / ts) % ts) / (ts - 1.);
-  scalar_t dim2 = (i % ts) / (ts - 1.);
-  if (1 < dim0 + dim1 + dim2) {
+  scalar_t dim0 = ((i / (ts * ts)) % ts) / (ts - 1.0f);
+  scalar_t dim1 = ((i / ts) % ts) / (ts - 1.0f);
+  scalar_t dim2 = (i % ts) / (ts - 1.0f);
+  if (1.0f < dim0 + dim1 + dim2) {
       float sum = dim0 + dim1 + dim2;
       dim0 /= sum;
       dim1 /= sum;
@@ -37,27 +36,19 @@ __global__ void load_textures_cuda_kernel(
       (face[2 * 0 + 0] * dim0 + face[2 * 1 + 0] * dim1 + face[2 * 2 + 0] * dim2) * (image_width - 1));
   const scalar_t pos_y = (
       (face[2 * 0 + 1] * dim0 + face[2 * 1 + 1] * dim1 + face[2 * 2 + 1] * dim2) * (image_height - 1));
-  if (1) {
-      /* bilinear sampling */
-      const scalar_t weight_x1 = pos_x - (int)pos_x;
-      const scalar_t weight_x0 = 1 - weight_x1;
-      const scalar_t weight_y1 = pos_y - (int)pos_y;
-      const scalar_t weight_y0 = 1 - weight_y1;
-      for (int k = 0; k < 3; k++) {
-          scalar_t c = 0;
-          c += image[((int)pos_y * image_width + (int)pos_x) * 3 + k] * (weight_x0 * weight_y0);
-          c += image[((int)(pos_y + 1) * image_width + (int)pos_x) * 3 + k] * (weight_x0 * weight_y1);
-          c += image[((int)pos_y * image_width + ((int)pos_x) + 1) * 3 + k] * (weight_x1 * weight_y0);
-          c += image[((int)(pos_y + 1)* image_width + ((int)pos_x) + 1) * 3 + k] * (weight_x1 * weight_y1);
-          texture[k] = c;
-      }
-  } else {
-      /* nearest neighbor */
-      const int pos_xi = round(pos_x);
-      const int pos_yi = round(pos_y);
-      for (int k = 0; k < 3; k++) {
-          texture[k] = image[(pos_yi * image_width + pos_xi) * 3 + k];
-      }
+  
+  /* Bilinear sampling */
+  const scalar_t weight_x1 = pos_x - floorf(pos_x);
+  const scalar_t weight_x0 = 1.0f - weight_x1;
+  const scalar_t weight_y1 = pos_y - floorf(pos_y);
+  const scalar_t weight_y0 = 1.0f - weight_y1;
+  for (int k = 0; k < 3; k++) {
+      scalar_t c = 0;
+      c += image[((int)pos_y * image_width + (int)pos_x) * 3 + k] * (weight_x0 * weight_y0);
+      c += image[((int)(pos_y + 1) * image_width + (int)pos_x) * 3 + k] * (weight_x0 * weight_y1);
+      c += image[((int)pos_y * image_width + ((int)pos_x) + 1) * 3 + k] * (weight_x1 * weight_y0);
+      c += image[((int)(pos_y + 1) * image_width + ((int)pos_x) + 1) * 3 + k] * (weight_x1 * weight_y1);
+      texture[k] = c;
   }
 }
 }
@@ -67,9 +58,8 @@ at::Tensor load_textures_cuda(
         at::Tensor faces,
         at::Tensor textures,
         at::Tensor is_update) {
-    // textures_size = size of the textures tensor
+    // Get tensor size attributes
     const auto textures_size = textures.numel();
-    // notice that texture_size != texture_size
     const auto texture_size = textures.size(1);
     const auto image_height = image.size(0);
     const auto image_width = image.size(1);
@@ -77,12 +67,12 @@ at::Tensor load_textures_cuda(
     const int threads = 1024;
     const dim3 blocks ((textures_size / 3 - 1) / threads + 1);
 
-    AT_DISPATCH_FLOATING_TYPES(image.type(), "load_textures_cuda", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(image.scalar_type(), "load_textures_cuda", ([&] {
       load_textures_cuda_kernel<scalar_t><<<blocks, threads>>>(
-          image.data<scalar_t>(),
-          faces.data<scalar_t>(),
-          is_update.data<int32_t>(),
-          textures.data<scalar_t>(),
+          image.data_ptr<scalar_t>(),
+          faces.data_ptr<scalar_t>(),
+          is_update.data_ptr<int32_t>(),
+          textures.data_ptr<scalar_t>(),
           textures_size,
           texture_size,
           image_height,
@@ -91,6 +81,6 @@ at::Tensor load_textures_cuda(
 
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) 
-            printf("Error in load_textures: %s\n", cudaGetErrorString(err));
+        printf("CUDA error in load_textures: %s\n", cudaGetErrorString(err));
     return textures;
 }
